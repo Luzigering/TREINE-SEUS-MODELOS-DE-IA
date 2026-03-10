c664792182ccd995b3172c8ad45760b143dc5499
Gering <93748581+Luzigering@users.noreply.github.com>
2026-03-10 16:05:07 -0300

    Allow all HTTP methods in CORS middleware

--- api/index.py
+++ api/index.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"], 
+    allow_methods=["*"], 
     allow_headers=["*"],
 )
 
